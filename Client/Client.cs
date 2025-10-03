9377ba5a3f941c0b356ec3e7be8b8784e4a52c6d
Henrik Bulskov <bulskov@ruc.dk>
2025-10-03 10:55:47 +0200

    create client server

---
+++ Client/Client.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+var client = new TcpClient();
+
+client.Connect(IPAddress.Loopback, 5000);
+
+var stream = client.GetStream();
+
+byte[] buffer = new byte[1024];
+
+var readCount = stream.Read(buffer);
+
+var msg = Encoding.UTF8.GetString(buffer, 0, readCount);
+
+
+Console.WriteLine(msg);
---
+++ Server/Server.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+int port = 5000;
+
+var server = new TcpListener(IPAddress.Loopback, port);
+
+server.Start();
+Console.WriteLine($"Server started on port {port}");
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    Console.WriteLine("Client connected");
+    HandleClient(client);
+}
+
+void HandleClient(TcpClient client)
+{
+    var stream = client.GetStream();
+
+    var msg = "Hello form server";
+
+    stream.Write(Encoding.UTF8.GetBytes(msg));
+
+} 
